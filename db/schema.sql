58f8a1a90bd67fb7863d0acfd2d3d071f714f106
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-11-22 20:21:05 -0500

    more sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,18 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- note - group by ID tables and then product level tables
+
 -- create the 'category' table
 CREATE TABLE category (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   category_name VARCHAR(30) NOT NULL,
+);
+
+-- create the 'tag' table
+CREATE TABLE tag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(30) NOT NULL,
 );
 
 -- create the 'product' table
@@ -17,6 +25,19 @@ CREATE TABLE product (
   price NUMERIC(10,5) NOT NULL,
   stock INT NOT NULL SET DEFAULT '0',
   category_id INT
+  FOREIGN KEY (category_id)
+  REFERENCES category(id)
+  ON DELETE SET NULL
+);
+
+-- create the 'productTag' table
+CREATE TABLE productTag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_id INT
+  FOREIGN KEY (category_id)
+  REFERENCES category(id)
+  ON DELETE SET NULL,
+  tag_id INT
   FOREIGN KEY (category_id)
   REFERENCES category(id)
   ON DELETE SET NULL
